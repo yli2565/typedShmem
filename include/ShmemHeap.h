6ef02dce5299bb9dd85d99d11b362f9acd9e3ffe
YUHAO <yli2565@wisc.edu>
2024-08-04 03:56:12 -0500

    roughly finish alloc and free. TODO: make a set of function to deal with linked list, take seperate bins into consideration. Do not use shmPter and capacity any more, add another layer of flexibility. heapHead() and heapTail()

--- include/ShmemHeap.h
+++ include/ShmemHeap.h
@@ -24,27 +24,51 @@ public:
          *
          * @return reference to the 8 bytes value at *(this)
          */
-        uintptr_t &getVal()
+        uintptr_t &val()
         {
             return *reinterpret_cast<uintptr_t *>(this);
         }
-        uintptr_t &getFwdOffset()
+        uintptr_t &fwdOffset()
         {
             return *(reinterpret_cast<uintptr_t *>(this) + 1);
         }
-        uintptr_t &getBckOffset()
+        uintptr_t &bckOffset()
         {
             return *(reinterpret_cast<uintptr_t *>(this) + 2);
         }
 
+        BlockHeader *getFooterPtr() const
+        {
+            return reinterpret_cast<BlockHeader *>(reinterpret_cast<uintptr_t>(this) + this->size() - sizeof(BlockHeader));
+        }
+
         BlockHeader *getFwdPtr() const
         {
-            return reinterpret_cast<BlockHeader *>(this->size_BPA - (this + 1)->size_BPA);
+            return reinterpret_cast<BlockHeader *>(reinterpret_cast<uintptr_t>(this) - (this + 1)->size_BPA);
         }
 
         BlockHeader *getBckPtr() const
         {
-            return reinterpret_cast<BlockHeader *>(this->size_BPA + (this + 2)->size_BPA);
+            return reinterpret_cast<BlockHeader *>(reinterpret_cast<uintptr_t>(this) + (this + 2)->size_BPA);
+        }
+
+        BlockHeader *removeFromFreeList()
+        {
+            BlockHeader *bckBlockHeader = this->getBckPtr();
+            BlockHeader *fwdBlockHeader = this->getFwdPtr();
+            fwdBlockHeader->bckOffset() = bckBlockHeader - fwdBlockHeader;
+            bckBlockHeader->fwdOffset() = bckBlockHeader - fwdBlockHeader;
+        }
+        BlockHeader *getPrevPtr() const
+        {
+            if (this->P())
+                throw std::runtime_error("Previous block is allocated. We should not call this function");
+            else
+                return reinterpret_cast<BlockHeader *>(reinterpret_cast<uintptr_t>(this) + (this - 1)->size_BPA);
+        }
+        BlockHeader *getNextPtr() const
+        {
+            return reinterpret_cast<BlockHeader *>(reinterpret_cast<uintptr_t>(this) + this->size());
         }
 
         bool B() const
@@ -62,6 +86,18 @@ public:
         size_t size() const
         {
             return size_BPA & ~0b111;
+        }
+        int wait(int timeout = -1) const
+        {
+            while (this->B())
+            {
+                if (timeout == 0)
+                    return ETIMEDOUT;
+                std::this_thread::sleep_for(std::chrono::milliseconds(1));
+                if (timeout > 0)
+                    timeout--;
+            }
+            return 0;
         }
     };
     const size_t unitSize = sizeof(void *);
@@ -91,6 +127,7 @@ public:
         size_t bestSize = std::numeric_limits<size_t>::max();
 
         // Best fit logic
+        // TODO: this is just a simple logic, it should be improved to use free block linked list
         while (current - this->shmPtr < this->capacity)
         {
             BlockHeader *currentHeader = reinterpret_cast<BlockHeader *>(current);
@@ -109,20 +146,38 @@ public:
 
         if (best != nullptr)
         {
+            // Lock the best fit block (Busy bit <- 1)
+            best->wait();
+            best->val() |= 0b100;
 
-            uintptr_t bestOffset = reinterpret_cast<uintptr_t>(best) - reinterpret_cast<uintptr_t>(shmPtr);
+            // Check if the best size < required size + 4
+            // In that case, we cannot split the block as a free block is minimum 4 bytes, we just increase the required size
+            if (bestSize < requiredSize + 4)
+            {
+                requiredSize = bestSize;
+            }
+
+            uintptr_t fwdOffset = best->fwdOffset();
+            uintptr_t bckOffset = best->bckOffset();
+
+            BlockHeader *fwdBlockHeader = best->getFwdPtr();
+            BlockHeader *bckBlockHeader = best->getBckPtr();
+
+            // Set busy bit
+            fwdBlockHeader->wait();
+            fwdBlockHeader->val() |= 0b100;
+            bckBlockHeader->wait();
+            bckBlockHeader->val() |= 0b100;
+
             // If the block we find is bigger than the required size, split it
             if (bestSize > requiredSize)
             {
                 BlockHeader *newBlockHeader = reinterpret_cast<BlockHeader *>(best + requiredSize);
                 BlockHeader *newBlockFooter = reinterpret_cast<BlockHeader *>(best + bestSize - sizeof(BlockHeader));
 
-                // Just for simplicity, .size_BPA is equivalent to static_cast<uintptr_t>(*(best + 1))
-                uintptr_t fwdOffset = best->getFwdOffset();
-                uintptr_t bckOffset = best->getBckOffset();
-
-                BlockHeader *fwdBlockHeader = best->getFwdPtr();
-                BlockHeader *bckBlockHeader = best->getBckPtr();
+                // Set busy bit
+                newBlockHeader->wait();
+                newBlockHeader->val() |= 0b100;
 
                 // If the fwd or bck block is best block itself, update to the new free block
                 if (fwdOffset == 0)
@@ -132,20 +187,120 @@ public:
 
                 // update the new block
                 // Size: bestSize - requiredSize; Busy: 1; Previous Allocated: 1; Allocated: 0
-                newBlockHeader->getVal() = (bestSize - requiredSize) | 0b010;
-                newBlockHeader->getFwdOffset() = newBlockHeader - fwdBlockHeader;
-                newBlockHeader->getBckOffset() = bckBlockHeader - newBlockHeader;
-                newBlockFooter->getVal() = bestSize - requiredSize;
+                newBlockHeader->val() = (bestSize - requiredSize) | 0b110;
+                newBlockHeader->fwdOffset() = newBlockHeader - fwdBlockHeader;
+                newBlockHeader->bckOffset() = bckBlockHeader - newBlockHeader;
+                newBlockFooter->val() = bestSize - requiredSize;
 
                 // update fwd and bck block's ptr
-                fwdBlockHeader->getBckOffset() = newBlockHeader - fwdBlockHeader;
-                bckBlockHeader->getFwdOffset() = bckBlockHeader - newBlockHeader;
+                fwdBlockHeader->bckOffset() = newBlockHeader - fwdBlockHeader;
+                bckBlockHeader->fwdOffset() = bckBlockHeader - newBlockHeader;
+
+                // Reset busy bit
+                newBlockHeader->val() &= ~0b100;
+            }
+            else
+            {
+                // Remove the best block from the double linked list
+                fwdBlockHeader->bckOffset() = bckBlockHeader - fwdBlockHeader;
+                bckBlockHeader->fwdOffset() = bckBlockHeader - fwdBlockHeader;
             }
 
-            // update the best bit block
-            
+            // Reset busy bit
+            fwdBlockHeader->val() &= ~0b100;
+            bckBlockHeader->val() &= ~0b100;
+
+            // update the best fit block
+            // Size: requiredSize; Busy: 0; Previous Allocated: not changed; Allocated: 1
+            best->val() = requiredSize & ~0b100 | (best->size_BPA & 0b010) | 0b001;
+
+            // update next block's p bit
+            if (static_cast<void *>(best + bestSize) < static_cast<void *>(this->shmPtr + this->capacity))
+            {
+                BlockHeader *nextBlockHeader = best + bestSize;
+                nextBlockHeader->val() |= 0b010;
+            }
+
+            return reinterpret_cast<Byte *>(best + sizeof(BlockHeader));
         }
-        void shfree(Byte * ptr);
+    }
+    int shfree(Byte *ptr)
+    {
+        if (ptr == nullptr)
+            return -1;
+
+        if (reinterpret_cast<uintptr_t>(ptr) % 8 != 0)
+            return -1;
+
+        if (ptr < this->shmPtr || ptr >= this->shmPtr + this->capacity)
+            return -1;
+
+        BlockHeader *header = reinterpret_cast<BlockHeader *>(ptr) - sizeof(BlockHeader);
+
+        // Set Busy bit
+        header->wait();
+        header->val() |= 0b100;
+
+        if (!header->A())
+            return -1;
+
+        // Set Allocated bit to 0
+        header->val() &= ~0b001;
+
+        // Immediate coalescing
+
+        // First coalesce with the previous block
+
+        BlockHeader *coalesceTarget = header;
+
+        // When coalescing with previous block, footer is unchanged
+        BlockHeader *newFooter = header->getFooterPtr();
+
+        while (!coalesceTarget->P())
+        {
+            BlockHeader *prevBlockHeader = coalesceTarget->getPrevPtr();
+            if (prevBlockHeader->A())
+                throw std::runtime_error("previous block is allocated, but following block's P bit is set");
+
+            // Set Busy bit
+            prevBlockHeader->wait();
+            prevBlockHeader->val() |= 0b100;
+
+            size_t newSize = prevBlockHeader->size() + sizeof(BlockHeader) + coalesceTarget->size();
+
+            // Size: newSize; Busy: 1; Previous Allocated: not changed; Allocated: 0
+            prevBlockHeader->size_BPA = newSize & 0b100 | (prevBlockHeader->size_BPA & 0b010) & ~0b001;
+            newFooter->val() = newSize;
+
+            // update free list ptr
+            prevBlockHeader->removeFromFreeList();
+
+            coalesceTarget = prevBlockHeader;
+        }
+
+        while (reinterpret_cast<uintptr_t>(coalesceTarget) < reinterpret_cast<uintptr_t>(this->shmPtr) + this->capacity && !coalesceTarget->getNextPtr()->A())
+        {
+            BlockHeader *nextBlockHeader = coalesceTarget->getNextPtr();
+
+            // Set Busy bit
+            nextBlockHeader->wait();
+            nextBlockHeader->val() |= 0b100;
+
+            newFooter = nextBlockHeader->getFooterPtr();
+
+            size_t newSize = coalesceTarget->size() + nextBlockHeader->size() + sizeof(BlockHeader);
+
+            // Size: newSize; Busy: 1; Previous Allocated: not changed; Allocated: 0
+            coalesceTarget->size_BPA = newSize | (coalesceTarget->size_BPA & 0b111);
+            newFooter->val() = newSize;
+
+            // update free list ptr
+            nextBlockHeader->removeFromFreeList();
+
+            // coalesceTarget unchanged;
+        }
+
+        return 0;
     }
 };
 
