4375be486f5302ecec91120bd2fc2fb3815a7e9e
Yuhao Li <yli2565@wisc.edu>
2024-08-04 00:12:19 -0500

    finish updating the new splitted free block

--- include/ShmemHeap.h
+++ include/ShmemHeap.h
@@ -17,6 +17,52 @@ public:
          * size of the block / Busy bit / Previous Allocated bit / Allocated bit
          */
         size_t size_BPA;
+
+        /**
+         * @brief Provide *(this) as a uintptr_t reference
+         * Similar to reinterpret cast BlockHeader ptr to a uintptr_t ptr
+         *
+         * @return reference to the 8 bytes value at *(this)
+         */
+        uintptr_t &getVal()
+        {
+            return *reinterpret_cast<uintptr_t *>(this);
+        }
+        uintptr_t &getFwdOffset()
+        {
+            return *(reinterpret_cast<uintptr_t *>(this) + 1);
+        }
+        uintptr_t &getBckOffset()
+        {
+            return *(reinterpret_cast<uintptr_t *>(this) + 2);
+        }
+
+        BlockHeader *getFwdPtr() const
+        {
+            return reinterpret_cast<BlockHeader *>(this->size_BPA - (this + 1)->size_BPA);
+        }
+
+        BlockHeader *getBckPtr() const
+        {
+            return reinterpret_cast<BlockHeader *>(this->size_BPA + (this + 2)->size_BPA);
+        }
+
+        bool B() const
+        {
+            return size_BPA & 0b1;
+        }
+        bool P() const
+        {
+            return size_BPA & 0b10;
+        }
+        bool A() const
+        {
+            return size_BPA & 0b100;
+        }
+        size_t size() const
+        {
+            return size_BPA & ~0b111;
+        }
     };
     const size_t unitSize = sizeof(void *);
     /**
@@ -48,8 +94,8 @@ public:
         while (current - this->shmPtr < this->capacity)
         {
             BlockHeader *currentHeader = reinterpret_cast<BlockHeader *>(current);
-            size_t blockSize = currentHeader->size_BPA & ~0b111;
-            if (currentHeader->size_BPA & 0b001 && blockSize >= requiredSize && blockSize < bestSize)
+            size_t blockSize = currentHeader->size();
+            if (currentHeader->A() && blockSize >= requiredSize && blockSize < bestSize)
             {
                 best = currentHeader;
                 bestSize = blockSize;
@@ -63,21 +109,44 @@ public:
 
         if (best != nullptr)
         {
+
+            uintptr_t bestOffset = reinterpret_cast<uintptr_t>(best) - reinterpret_cast<uintptr_t>(shmPtr);
             // If the block we find is bigger than the required size, split it
             if (bestSize > requiredSize)
             {
                 BlockHeader *newBlockHeader = reinterpret_cast<BlockHeader *>(best + requiredSize);
                 BlockHeader *newBlockFooter = reinterpret_cast<BlockHeader *>(best + bestSize - sizeof(BlockHeader));
-                // Size: bestSize - requiredSize; Busy: 1; Previous Allocated: 1; Allocated: 0
-                newBlockHeader->size_BPA = (bestSize - requiredSize) | 0b010;
 
-                void *fwdPtr = reinterpret_cast<void *>(best + 1);
-                void *bckPtr = reinterpret_cast<void *>(best + 2);
+                // Just for simplicity, .size_BPA is equivalent to static_cast<uintptr_t>(*(best + 1))
+                uintptr_t fwdOffset = best->getFwdOffset();
+                uintptr_t bckOffset = best->getBckOffset();
+
+                BlockHeader *fwdBlockHeader = best->getFwdPtr();
+                BlockHeader *bckBlockHeader = best->getBckPtr();
+
+                // If the fwd or bck block is best block itself, update to the new free block
+                if (fwdOffset == 0)
+                    fwdBlockHeader = newBlockHeader;
+                if (bckOffset == 0)
+                    bckBlockHeader = newBlockHeader;
+
+                // update the new block
+                // Size: bestSize - requiredSize; Busy: 1; Previous Allocated: 1; Allocated: 0
+                newBlockHeader->getVal() = (bestSize - requiredSize) | 0b010;
+                newBlockHeader->getFwdOffset() = newBlockHeader - fwdBlockHeader;
+                newBlockHeader->getBckOffset() = bckBlockHeader - newBlockHeader;
+                newBlockFooter->getVal() = bestSize - requiredSize;
+
+                // update fwd and bck block's ptr
+                fwdBlockHeader->getBckOffset() = newBlockHeader - fwdBlockHeader;
+                bckBlockHeader->getFwdOffset() = bckBlockHeader - newBlockHeader;
             }
+
+            // update the best bit block
+            
         }
         void shfree(Byte * ptr);
-
-    private:
-    };
+    }
+};
 
 #endif // SHMEM_HEAP_H
